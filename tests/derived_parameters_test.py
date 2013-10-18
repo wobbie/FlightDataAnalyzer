1c77f2c4ebcff861be82c96a692967202c31ed52
Marek Petlicki <marek.petlicki@flightdataservices.com>
2013-10-18 11:05:16 +0100

    Fix: removed test for nonexisting parametrer

--- tests/derived_parameters_test.py
+++ tests/derived_parameters_test.py
@@ -136,7 +136,6 @@ from analysis_engine.derived_parameters import (
     SidestickAngleFO,
     SlatSurface,
     Speedbrake,
-    Spoiler,
     VerticalSpeed,
     VerticalSpeedForFlightPhases,
     RateOfTurn,
@@ -3870,29 +3869,6 @@ class TestSpeedbrake(unittest.TestCase):
     def test_derive(self):
         self.assertTrue(False, msg='Test not implemented.')
 
-
-class TestSpoiler(unittest.TestCase):
-    def test_can_operate(self):
-        family = Attribute('Family', 'B787')
-        self.assertTrue(Spoiler.can_operate(('Spoiler (1)',), family=family))
-        self.assertTrue(Spoiler.can_operate(('Spoiler (14)',), family=family))
-        self.assertTrue(Spoiler.can_operate(('Spoiler (1)', 'Spoiler (14)'),
-                                            family=family))
-        family = Attribute('Family', 'A320')
-        self.assertFalse(Spoiler.can_operate(('Spoiler (1)', 'Spoiler (14)'),
-                                             family=family))
-        family = Attribute('Family', 'G-V')
-        self.assertTrue(Spoiler.can_operate(('Spoiler (L)', 'Spoiler (R)'),
-                                             family=family))
-    
-    def test_derive_Gulfstream(self):
-        spoiler = P('Spoiler (L)', array=np.ma.array([1,2,1,2,3,2,1,2,35,40,2,1.0]))
-        family = A('Family', value='G-V')
-        spoil = Spoiler()
-        spoil.derive(None, None, spoiler, None, None, None, None, None, family)
-        expected = np.array([1,2,1,2,3,2,1,2,35,40,2,1.0])
-        np.testing.assert_array_equal(spoil.array.data, expected)
-        
 
 class TestSAT(unittest.TestCase):
     # Note: the core function machtat2sat is tested by the library test.
