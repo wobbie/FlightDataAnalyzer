4a7b0bf34b2f8037a0ee6b6cbacb18546f261a52
Chris Jesse <chris.jesse@flightdataservices.com>
2013-10-29 15:38:46 +0000

    Catch ValueError in slices_between repair mask and handle

--- analysis_engine/library.py
+++ analysis_engine/library.py
@@ -5006,10 +5006,12 @@ def slices_between(array, min_, max_):
     :returns: Slices where the array is above a certain value.
     :rtype: list of slice
     '''
-    if len(array) == 0:
+    if np.ma.count(array) == 0:
         return array, []
-    repaired_array = repair_mask(array)
-    if repaired_array is None: # Array length is too short to be repaired.
+    try:
+        repaired_array = repair_mask(array)
+    except ValueError:
+        # data is entirely masked or too short to be repaired
         return array, []
     # Slice through the array at the top and bottom of the band of interest
     band = np.ma.masked_outside(repaired_array, min_, max_)
