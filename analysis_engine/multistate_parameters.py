42ce15b41b68ef60dbd6f2f0eb1f054eeab53441
Dave Jesse <dave.jesse@flightdataservices.com>
2013-11-15 11:26:01 +0000

    CRJ Speedbrake settings allow for different parameters to be available (some aircraft have a useable Speedbrake Handle, while others do not).

--- analysis_engine/multistate_parameters.py
+++ analysis_engine/multistate_parameters.py
@@ -1735,7 +1735,7 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
         elif family_name == 'Learjet':
             self.array = self.learjet_speedbrake(spdsw)
 
-        elif family_name == 'G-V':
+        elif family_name in ['G-V', 'CL-600'] and spdbrk:
             # On the test aircraft SE-RDY the Speedbrake stored 0 at all
             # times and Speedbrake Handle was unresponsive with small numeric
             # variation. The Speedbrake (L) & (R) responded normally so we
@@ -1744,7 +1744,7 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
                                      'Stowed',
                                      'Deployed/Cmd Up')
 
-        elif family_name in['Global', 'CL-600', 'CRJ 100/200', 'ERJ-135/145']:
+        elif family_name in['Global', 'CRJ 100/200', 'ERJ-135/145', 'CL-600'] and handle:
             # No valid data seen for this type to date....
             logger.warning(
                 'SpeedbrakeSelected: algorithm for family `%s` is undecided, '
