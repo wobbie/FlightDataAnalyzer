70673b72a0de0683b87a2dc68fedccc17b89621d
Chris Jesse <chris.jesse@flightdataservices.com>
2014-01-20 09:50:36 +0000

    Flight number fix where no values found

--- analysis_engine/flight_attribute.py
+++ analysis_engine/flight_attribute.py
@@ -188,6 +188,7 @@ class FlightNumber(FlightAttributeNode):
     """
     "Airline route flight number"
     name = 'FDR Flight Number'
+    
     def derive(self, num=P('Flight Number')):
         # Q: Should we validate the flight number?
         if num.array.dtype.type is np.string_:
@@ -218,7 +219,7 @@ class FlightNumber(FlightAttributeNode):
         else:
             self.warning("Only %d out of %d flight numbers were the same."\
                          " Flight Number attribute will be set as None.",
-                         count, len(num.array))
+                         count or 0, len(num.array))
             self.set_flight_attr(None)
             return
 
