9e6ae78028fed6659f2467e6358aab29d559a3bc
Glen Kirkup <glen.kirkup@flightdataservices.com>
2014-02-14 15:36:22 +0000

    Force frequency and offset in GrossWeight to avoid interpolation error.

--- analysis_engine/derived_parameters.py
+++ analysis_engine/derived_parameters.py
@@ -3163,6 +3163,8 @@ class GrossWeight(DerivedParameterNode):
     '''
     Derive gross weight from Zero Fuel Weight and Fuel Qty.
     '''
+    align_frequency = 1
+    align_offset = 0
     units = ut.KG
     
     @classmethod
