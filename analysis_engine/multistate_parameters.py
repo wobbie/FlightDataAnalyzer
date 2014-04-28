da1708f17585b52d6ee703f6fb26d3f553af3d61
Rob Parker <robert.parker@flightdataservices.com>
2014-04-28 15:38:46 +0100

    added G-IV to speedbrake selected

--- analysis_engine/multistate_parameters.py
+++ analysis_engine/multistate_parameters.py
@@ -2017,7 +2017,8 @@ class SpeedbrakeSelected(MultistateDerivedParameterNode):
         elif family_name == 'Learjet':
             self.array = self.learjet_speedbrake(spdsw)
 
-        elif family_name in ['G-V',
+        elif family_name in ['G-IV',
+                             'G-V',
                              'CL-600',
                              'BAE 146',
                              'ERJ-170/175',
