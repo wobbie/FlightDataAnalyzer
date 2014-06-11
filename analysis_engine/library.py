wobbie/FlightDataAnalyzer

commit a34002511733e099ed209b04ab529849ac950704

changes to _value min/max/max abs values to better handle start and stop edges
note: could use further further improvements and refactoring but upcoming intervals feature will resolve these issues in a cleaner manor

analysis_engine/library.py
@@ -1823,8 +1823,11 @@ def find_edges_on_state_change(state, array, change='entering', phase=None, min_
         offset = _slice.start - 0.5
         state_periods = np.ma.clump_unmasked(
             np.ma.masked_not_equal(array[_slice], array.state[state]))
-        # ignore small periods where slice is in state, then remove small gaps where slices are not in state
-        state_periods = slices_remove_small_slices(state_periods, count=min_samples)
+        # ignore small periods where slice is in state, then remove small
+        # gaps where slices are not in state
+        # we are taking 1 away from min_samples here as
+        # slices_remove_small_slices removes slices of less than count
+        state_periods = slices_remove_small_slices(state_periods, count=min_samples - 1)
         state_periods = slices_remove_small_gaps(state_periods, count=min_samples)
         edge_list = []
         for period in state_periods:
@@ -3571,7 +3574,7 @@ def slices_remove_small_gaps(slice_list, time_limit=10, hz=1, count=None):
     
     :returns: slice list.
     '''
-    if count:
+    if count is not None:
         sample_limit = count
     else:
         sample_limit = time_limit * hz
@@ -3604,7 +3607,7 @@ def slices_remove_small_slices(slice_list, time_limit=10, hz=1, count=None):
 
     :returns: slice list.
     '''
-    if count:
+    if count is not None:
         sample_limit = count
     else:
         sample_limit = time_limit * hz
@@ -3829,18 +3832,12 @@ def max_abs_value(array, _slice=slice(None), start_edge=None, stop_edge=None):
 
     :returns: Value named tuple of index and value.
     """
-    index, value = max_value(np.ma.abs(array), _slice)
-    # If start or stop edges are given, check these extreme (interpolated) values.
-    if start_edge:
-        edge_value = abs(value_at_index(array, start_edge) or 0)
-        if edge_value and edge_value > value:
-            index = start_edge
-            value = edge_value
-    if stop_edge:
-        edge_value = abs(value_at_index(array, stop_edge) or 0)
-        if edge_value and edge_value > value:
-            index = stop_edge
-            value = edge_value
+    #slice_start = start_edge if start_edge is not None else _slice.start
+    #slice_stop = stop_edge if stop_edge is not None else _slice.stop
+    #max_val_slice = slice(slice_start, slice_stop, _slice.step)
+    index, value = max_value(np.ma.abs(array), _slice,
+                          start_edge=start_edge, stop_edge=stop_edge)
+
     if value is None:
         return Value(None, None)
     else:
@@ -3863,18 +3860,12 @@ def max_value(array, _slice=slice(None), start_edge=None, stop_edge=None):
 
     :returns: Value named tuple of index and value.
     """
-    index, value = _value(array, _slice, np.ma.argmax)
-    # If start or stop edges are given, check these extreme (interpolated) values.
-    if start_edge:
-        edge_value = value_at_index(array, start_edge)
-        if edge_value and edge_value > value:
-            index = start_edge
-            value = edge_value
-    if stop_edge:
-        edge_value = value_at_index(array, stop_edge)
-        if edge_value and edge_value > value:
-            index = stop_edge
-            value = edge_value
+    #slice_start = start_edge if start_edge is not None else _slice.start
+    #slice_stop = stop_edge if stop_edge is not None else _slice.stop
+    #max_val_slice = slice(slice_start, slice_stop, _slice.step)
+    index, value = _value(array, _slice, np.ma.argmax,
+                          start_edge=start_edge, stop_edge=stop_edge)
+
     return Value(index, value)
 
 
@@ -3907,18 +3898,12 @@ def min_value(array, _slice=slice(None), start_edge=None, stop_edge=None):
 
     :returns: Value named tuple of index and value.
     """
-    index, value = _value(array, _slice, np.ma.argmin)
-    # If start or stop edges are given, check these extreme (interpolated) values.
-    if start_edge:
-        edge_value = value_at_index(array, start_edge)
-        if edge_value and edge_value < value:
-            index = start_edge
-            value = edge_value
-    if stop_edge:
-        edge_value = value_at_index(array, stop_edge)
-        if edge_value and edge_value < value:
-            index = stop_edge
-            value = edge_value
+    #slice_start = start_edge if start_edge is not None else _slice.start
+    #slice_stop = stop_edge if stop_edge is not None else _slice.stop
+    #min_val_slice = slice(slice_start, slice_stop, _slice.step)
+    index, value = _value(array, _slice, np.ma.argmin,
+                          start_edge=start_edge, stop_edge=stop_edge)
+
     return Value(index, value)
 
 
@@ -6636,17 +6621,51 @@ def index_at_value_or_level_off(array, value, _slice, abs_threshold=None):
             return find_toc_tod(array, rev_slice, 1, 'Descent')
 
 
-def _value(array, _slice, operator):
+def _value(array, _slice, operator, start_edge=None, stop_edge=None):
     """
     Applies logic of min_value and max_value across the array slice.
     """
+    start_result = np.nan
+    stop_result = np.nan
+    slice_start = _slice.start
+    slice_stop = _slice.stop
+    start_idx = slice_start
+    stop_idx = slice_stop
+
+    # have we got sections or slices
+    if slice_start and slice_start % 1:
+        start_edge = slice_start
+        slice_start = ceil(slice_start)
+    if slice_stop and slice_stop % 1:
+        stop_edge = slice_stop
+        slice_stop = floor(slice_stop)
+
+    # get start_edge and stop_edge values if required
+    if start_edge:
+        start_result = value_at_index(array, start_edge)
+        start_result = start_result if start_result is not None and start_result is not np.ma.masked else np.nan
+        start_idx = start_edge
+    if stop_edge:
+        stop_result = value_at_index(array, stop_edge)
+        stop_result = stop_result if stop_result is not None and stop_result is not np.ma.masked else np.nan
+        stop_idx = stop_edge
+
+    search_slice = slice(slice_start, slice_stop, _slice.step)
+
     if _slice.step and _slice.step < 0:
         raise ValueError("Negative step not supported")
-    if np.ma.count(array[_slice]):
+    if np.ma.count(array[search_slice]):
         # floor the start position as it will have been floored during the slice
-        index = operator(array[_slice]) + floor(_slice.start or 0) * (_slice.step or 1)
-        value = array[index]
-        return Value(index, value)
+        value_index = operator(array[search_slice]) + floor(search_slice.start or 0) * (search_slice.step or 1)
+        value = array[value_index]
+        indexes = (start_idx, value_index, stop_idx)
+        values = (start_result, value, stop_result)
+        values_mask = [True if x is np.nan else False for x in values]
+        # test start / stop edgge
+        index = operator(np.ma.array(data=values, mask=values_mask))
+
+        return Value(indexes[index], values[index])
+    
     else:
         return Value(None, None)
 

tests/key_point_value_test.py
@@ -655,7 +655,11 @@ class CreateKPVsWhereTest(NodeTest):
         if not hasattr(self, 'expected'):
             self.expected = []
             if self.phases:
-                slices = [p.slice for p in self.phases]
+                # TODO: remove after intervals have been implemented
+                if hasattr(self, 'complex_where'):
+                    slices = self.phases.get_slices()
+                else:
+                    slices = [p.slice for p in self.phases]
             else:
                 slices = [slice(None)]
 
@@ -822,7 +826,7 @@ class ILSTest(NodeTest):
             array=np.ma.array([108.5] * 6 + [114.05] * 4),
         )
         ils_frequency.array[0:10:2] = np.ma.masked
-        ils_ests = buildsection('ILS Localizer Established', 2, 9)
+        ils_ests = buildsection('ILS Localizer Established', 2, 8)
         return ils_frequency, ils_ests
 
     def prepare__glideslope__basic(self):
@@ -835,7 +839,7 @@ class ILSTest(NodeTest):
             # Altitude from 1875 to 325 ft in 63 steps.
             array=np.ma.array((75 - np.arange(63)) * 25),
         )
-        ils_ests = buildsection('ILS Glideslope Established', 2, 63)
+        ils_ests = buildsection('ILS Glideslope Established', 2, 62)
         return ils_glideslope, alt_aal, ils_ests
 
     def prepare__glideslope__four_peaks(self):
@@ -848,7 +852,7 @@ class ILSTest(NodeTest):
             # Altitude from 1875 to 325 ft in 63 steps.
             array=np.ma.array((75 - np.arange(63)) * 25),
         )
-        ils_ests = buildsection('ILS Glideslope Established', 2, 56)
+        ils_ests = buildsection('ILS Glideslope Established', 2, 55)
         return ils_glideslope, alt_aal, ils_ests
 
     def prepare__localizer__basic(self):
@@ -2286,7 +2290,7 @@ class TestAirspeedWithFlapDuringClimbMax(unittest.TestCase, NodeTest):
         mapping = {int(f): str(f) for f in np.ma.unique(array)}
         flap_exc_trans = M('Flap Excluding Transition', array, values_mapping=mapping)
         airspeed = P('Airspeed', np.ma.arange(0, 100, 10))
-        climb = buildsection('Climbing', 2, 8)
+        climb = buildsection('Climbing', 2, 7)
         node = self.node_class()
         node.derive(None, None, flap_inc_trans, flap_exc_trans, airspeed, climb)
         self.assertEqual(node.get_ordered_by_index(), [
@@ -2333,7 +2337,7 @@ class TestAirspeedWithFlapDuringDescentMax(unittest.TestCase, NodeTest):
         mapping = {int(f): str(f) for f in np.ma.unique(array)}
         flap_exc_trans = M('Flap Excluding Transition', array, values_mapping=mapping)
         airspeed = P('Airspeed', np.ma.arange(100, 0, -10))
-        desc = buildsection('Descending', 2, 8)
+        desc = buildsection('Descending', 2, 7)
         node = self.node_class()
         node.derive(None, None, flap_inc_trans, flap_exc_trans, airspeed, desc)
         self.assertEqual(node.get_ordered_by_index(), [
@@ -2376,7 +2380,7 @@ class TestAirspeedMinusFlapManoeuvreSpeedWithFlapDuringDescentMin(unittest.TestC
         mapping = {int(f): str(f) for f in np.ma.unique(array)}
         flap = M('Flap Lever', array, values_mapping=mapping)
         airspeed = P('Airspeed', np.ma.arange(100, 0, -10))
-        descents = buildsection('Descent To Flare', 2, 8)
+        descents = buildsection('Descent To Flare', 2, 7)
         node = self.node_class()
         node.derive(flap, None, airspeed, descents)
         self.assertEqual(node.get_ordered_by_index(), [
@@ -6569,7 +6573,7 @@ class TestHeadingDuringTakeoff(unittest.TestCase, NodeTest):
 
     def test_derive_basic(self):
         head = P('Heading Continuous',np.ma.array([0,2,4,7,9,8,6,3]))
-        toff = buildsection('Takeoff', 2,6)
+        toff = buildsection('Takeoff', 2,5)
         kpv = HeadingDuringTakeoff()
         kpv.derive(head, toff)
         expected = [KeyPointValue(index=4, value=7.5,
@@ -6578,7 +6582,7 @@ class TestHeadingDuringTakeoff(unittest.TestCase, NodeTest):
 
     def test_derive_modulus(self):
         head = P('Heading Continuous',np.ma.array([0,2,4,7,9,8,6,3])*-1.0)
-        toff = buildsection('Takeoff', 2,6)
+        toff = buildsection('Takeoff', 2,5)
         kpv = HeadingDuringTakeoff()
         kpv.derive(head, toff)
         expected = [KeyPointValue(index=4, value=360-7.5,
@@ -6594,7 +6598,7 @@ class TestHeadingTrueDuringTakeoff(unittest.TestCase, NodeTest):
 
     def test_derive_basic(self):
         head = P('Heading True Continuous',np.ma.array([0,2,4,7,9,8,6,3]))
-        toff = buildsection('Takeoff', 2,6)
+        toff = buildsection('Takeoff', 2,5)
         kpv = self.node_class()
         kpv.derive(head, toff)
         expected = [KeyPointValue(index=4, value=7.5,
@@ -6603,7 +6607,7 @@ class TestHeadingTrueDuringTakeoff(unittest.TestCase, NodeTest):
 
     def test_derive_modulus(self):
         head = P('Heading True Continuous',np.ma.array([0,2,4,7,9,8,6,3])*-1.0)
-        toff = buildsection('Takeoff', 2,6)
+        toff = buildsection('Takeoff', 2,5)
         kpv = self.node_class()
         kpv.derive(head, toff)
         expected = [KeyPointValue(index=4, value=360-7.5,
@@ -6619,7 +6623,7 @@ class TestHeadingDuringLanding(unittest.TestCase, NodeTest):
     def test_derive_basic(self):
         head = P('Heading Continuous',np.ma.array([0,1,2,3,4,5,6,7,8,9,10,-1,-1,
                                                    7,-1,-1,-1,-1,-1,-1,-1,-10]))
-        landing = buildsection('Landing',5,15)
+        landing = buildsection('Landing',5,14)
         head.array[13] = np.ma.masked
         kpv = HeadingDuringLanding()
         kpv.derive(head, landing)
@@ -6639,7 +6643,7 @@ class TestHeadingTrueDuringLanding(unittest.TestCase, NodeTest):
         head = P('Heading True Continuous',
                  np.ma.array([0,1,2,3,4,5,6,7,8,9,10,-1,-1,
                               7,-1,-1,-1,-1,-1,-1,-1,-10]))
-        landing = buildsection('Landing', 5, 15)
+        landing = buildsection('Landing', 5, 14)
         head.array[13] = np.ma.masked
         kpv = HeadingDuringLanding()
         kpv.derive(head, landing)
@@ -8853,7 +8857,7 @@ class TestTaxiInDuration(unittest.TestCase):
         self.assertEqual(opts, [('Taxi In',)])
         
     def test_derive(self):
-        taxi_ins = buildsections('Taxi In', [5, 10], [20, 30])
+        taxi_ins = buildsections('Taxi In', [5, 9], [20, 29])
         node = TaxiInDuration()
         node.derive(taxi_ins)
         self.assertEqual(len(node), 2)
@@ -8871,7 +8875,7 @@ class TestTaxiOutDuration(unittest.TestCase):
         self.assertEqual(opts, [('Taxi Out',)])
         
     def test_derive(self):
-        taxi_outs = buildsections('Taxi Out', [35, 67])
+        taxi_outs = buildsections('Taxi Out', [35, 66])
         node = TaxiOutDuration()
         node.derive(taxi_outs)
         self.assertEqual(len(node), 1)
@@ -8966,14 +8970,14 @@ class TestTAWSTerrainWarningDuration(unittest.TestCase, NodeTest):
         self.assertEqual(node[0].index, 15)
         self.assertEqual(node[0].value, 10)
         self.assertEqual(node[1].index, 30)
-        self.assertEqual(node[1].value, 3)
+        self.assertEqual(node[1].value, 4)
         node = self.node_class()
         node.derive(taws_terrain, taws_terrain_warning, airborne)
         self.assertEqual(len(node), 2)
         self.assertEqual(node[0].index, 12)
         self.assertEqual(node[0].value, 13)
         self.assertEqual(node[1].index, 30)
-        self.assertEqual(node[1].value, 3)
+        self.assertEqual(node[1].value, 4)
 
 
 class TestTAWSTerrainPullUpWarningDuration(unittest.TestCase, NodeTest):
@@ -9148,6 +9152,8 @@ class TestTAWSTerrainAheadPullUpDuration(unittest.TestCase,
 class TestTAWSWindshearCautionBelow1500FtDuration(unittest.TestCase,
                                                   CreateKPVsWhereTest):
     def setUp(self):
+        # TODO: remove after intervals have been implemented 
+        self.complex_where = True
         self.param_name = 'TAWS Windshear Caution'
         self.phase_name = 'Fast'
         self.node_class = TAWSWindshearCautionBelow1500FtDuration
@@ -9169,6 +9175,8 @@ class TestTAWSWindshearCautionBelow1500FtDuration(unittest.TestCase,
 class TestTAWSWindshearSirenBelow1500FtDuration(unittest.TestCase,
                                                 CreateKPVsWhereTest):
     def setUp(self):
+        # TODO: remove after intervals have been implemented 
+        self.complex_where = True
         self.param_name = 'TAWS Windshear Siren'
         self.phase_name = 'Fast'
         self.node_class = TAWSWindshearSirenBelow1500FtDuration
@@ -9234,7 +9242,7 @@ class TestTCASTAWarningDuration(unittest.TestCase, NodeTest):
         tcas = M(
             'TCAS Combined Control', array=np.ma.array([0,1,2,3,4,6,6,6,4,5]),
             values_mapping=values_mapping)
-        airborne = buildsection('Airborne', 2, 7)
+        airborne = buildsection('Airborne', 2, 6)
         node = self.node_class()
         node.derive(None, tcas, airborne)
         self.assertEqual([KeyPointValue(5.0, 2.0, 'TCAS TA Warning Duration')],
@@ -9280,7 +9288,7 @@ class TestTCASRAWarningDuration(unittest.TestCase, NodeTest):
         tcas = M(
             'TCAS Combined Control', array=np.ma.array([0,1,2,3,4,5,4,5,6]),
             values_mapping=values_mapping)
-        airborne = buildsection('Airborne', 2, 7)
+        airborne = buildsection('Airborne', 2, 6)
         node = self.node_class()
         node.derive(None, tcas, airborne)
         self.assertEqual([KeyPointValue(2, 5.0, 'TCAS RA Warning Duration')],

tests/key_time_instance_test.py
@@ -215,7 +215,7 @@ class TestClimbAccelerationStart(unittest.TestCase):
     def test_derive_basic(self):
         array = np.ma.concatenate(([155]*15, [180]*20))
         spd_sel = Parameter('Airspeed Selected', array=array)
-        init_climbs = buildsection('Initial Climb', 5, 30)
+        init_climbs = buildsection('Initial Climb', 5, 29)
         node = self.node_class()
         node.derive(spd_sel, init_climbs, None, None)
         self.assertEqual(len(node), 1)
@@ -224,7 +224,7 @@ class TestClimbAccelerationStart(unittest.TestCase):
     def test_derive_spd_unchanged(self):
         array = np.ma.array([155]*35)
         spd_sel = Parameter('Airspeed Selected', array=array)
-        init_climbs = buildsection('Initial Climb', 5, 30)
+        init_climbs = buildsection('Initial Climb', 5, 29)
         node = self.node_class()
         node.derive(spd_sel, init_climbs, None, None)
         self.assertEqual(len(node), 0)
@@ -233,7 +233,7 @@ class TestClimbAccelerationStart(unittest.TestCase):
         array = np.ma.concatenate(([155]*15, [180]*20))
         array[5:30] = np.ma.masked
         spd_sel = Parameter('Airspeed Selected', array=array)
-        init_climbs = buildsection('Initial Climb', 5, 30)
+        init_climbs = buildsection('Initial Climb', 5, 29)
         node = self.node_class()
         node.derive(spd_sel, init_climbs, None, None)
         self.assertEqual(len(node), 0)
@@ -538,8 +538,8 @@ class TestLandingTurnOffRunway(unittest.TestCase):
     def test_landing_turn_off_runway_basic(self):
         instance = LandingTurnOffRunway()
         head = P('Heading Continuous', np.ma.array([0]*30))
-        fast = buildsection('Fast', 0, 20)
-        land = buildsection('Landing', 10, 26)
+        fast = buildsection('Fast', 0, 19)
+        land = buildsection('Landing', 10, 25)
         instance.derive(head, land, fast)
         expected = [KeyTimeInstance(index=26, name='Landing Turn Off Runway')]
         self.assertEqual(instance, expected)
@@ -547,8 +547,8 @@ class TestLandingTurnOffRunway(unittest.TestCase):
     def test_landing_turn_off_runway_curved(self):
         instance = LandingTurnOffRunway()
         head = P('Heading Continuous',np.ma.array([0]*70+range(20)))
-        fast = buildsection('Fast',0,65)
-        land = buildsection('Landing',60,87)
+        fast = buildsection('Fast',0,64)
+        land = buildsection('Landing',60,86)
         instance.derive(head, land, fast)
         expected = [KeyTimeInstance(index=73, name='Landing Turn Off Runway')]
         self.assertEqual(instance, expected)
@@ -556,8 +556,8 @@ class TestLandingTurnOffRunway(unittest.TestCase):
     def test_landing_turn_off_runway_curved_left(self):
         instance = LandingTurnOffRunway()
         head = P('Heading Continuous',np.ma.array([0]*70+range(20))*-1.0)
-        fast = buildsection('Fast',0,65)
-        land = buildsection('Landing',60,87)
+        fast = buildsection('Fast',0,64)
+        land = buildsection('Landing',60,86)
         instance.derive(head, land, fast)
         expected = [KeyTimeInstance(index=73, name='Landing Turn Off Runway')]
         self.assertEqual(instance, expected)
@@ -778,7 +778,7 @@ class TestTopOfDescent(unittest.TestCase):
         alt_data = np.ma.array(range(0,800,100)+[800]*5)
         alt = Parameter('Altitude STD', np.ma.array(alt_data))
         phase = TopOfDescent()
-        in_air = buildsection('Climb Cruise Descent',0,len(alt.array))
+        in_air = buildsection('Climb Cruise Descent',0,len(alt.array)-1)
         phase.derive(alt, in_air)
         expected = []
         self.assertEqual(phase, expected)
@@ -1361,7 +1361,7 @@ class TestFlapRetractionWhileAirborne(unittest.TestCase, NodeTest):
         self.flap_synth = M(name='Flap Lever (Synthetic)', array=array, values_mapping=mapping)
 
     def test_derive(self):
-        airborne = buildsection('Airborne', 2, 12)
+        airborne = buildsection('Airborne', 2, 11)
         name = self.node_class.get_name()
         node = self.node_class()
         node.derive(self.flap_lever, None, airborne)
@@ -1402,7 +1402,7 @@ class TestFlapRetractionDuringGoAround(unittest.TestCase, NodeTest):
         self.flap_synth = M(name='Flap Lever (Synthetic)', array=array, values_mapping=mapping)
 
     def test_derive(self):
-        airborne = buildsection('Go Around', 2, 12)
+        airborne = buildsection('Go Around', 2, 11)
         name = self.node_class.get_name()
         node = self.node_class()
         node.derive(self.flap_lever, None, airborne)
@@ -1514,7 +1514,7 @@ class TestLocalizerEstablishedEnd(unittest.TestCase):
             LocalizerEstablishedEnd.get_operational_combinations())
 
     def test_derive(self):
-        ils = buildsection('ILS Localizer Established', 10, 20)
+        ils = buildsection('ILS Localizer Established', 10, 19)
         expected = [
             KeyTimeInstance(index=20, name='Localizer Established End')]
         les = LocalizerEstablishedEnd()

tests/library_test.py
@@ -1952,17 +1952,16 @@ class TestFindEdgesOnStateChange(unittest.TestCase):
         
         gear_down_indexes = find_edges_on_state_change(
             'Down', gear_down, change='entering', phase=[slice(0, touchdown.index)], min_samples=3)
-        self.assertEqual(len(gear_down_indexes), 1)
-        last_state_change = gear_down_indexes[-1]
-        self.assertEqual(last_state_change, 19.5)
-        
-        gear_down_indexes = find_edges_on_state_change(
-                    'Down', gear_down, change='entering', phase=[slice(0, touchdown.index)], min_samples=2)
+        self.assertEqual(len(gear_down_indexes), 2)
         self.assertEqual(gear_down_indexes, [11.5, 19.5])
         
         gear_down_indexes = find_edges_on_state_change(
+                    'Down', gear_down, change='entering', phase=[slice(0, touchdown.index)], min_samples=2)
+        self.assertEqual(gear_down_indexes, [6.5, 11.5, 19.5])
+        
+        gear_down_indexes = find_edges_on_state_change(
                     'Down', gear_down, change='entering', phase=[slice(0, touchdown.index)], min_samples=1)
-        self.assertEqual(gear_down_indexes, [6.5, 11.5, 19.5, 25.5])
+        self.assertEqual(gear_down_indexes, [3.5, 6.5, 11.5, 19.5, 25.5])
                 
         
         

commit e20150b2b2d608a9e44e1e7bce5b54a4ec26c9bd

fix to _values to correct issue with mapped array introduced in commit a34002511733e099ed209b04ab529849ac950704

analysis_engine/library.py
@@ -6640,31 +6640,28 @@ def _value(array, _slice, operator, start_edge=None, stop_edge=None):
         stop_edge = slice_stop
         slice_stop = floor(slice_stop)
 
-    # get start_edge and stop_edge values if required
-    if start_edge:
-        start_result = value_at_index(array, start_edge)
-        start_result = start_result if start_result is not None and start_result is not np.ma.masked else np.nan
-        start_idx = start_edge
-    if stop_edge:
-        stop_result = value_at_index(array, stop_edge)
-        stop_result = stop_result if stop_result is not None and stop_result is not np.ma.masked else np.nan
-        stop_idx = stop_edge
+    values = []
 
     search_slice = slice(slice_start, slice_stop, _slice.step)
 
     if _slice.step and _slice.step < 0:
         raise ValueError("Negative step not supported")
     if np.ma.count(array[search_slice]):
+        # get start_edge and stop_edge values if required
+        if start_edge:
+            start_result = value_at_index(array, start_edge)
+            if start_result is not None and start_result is not np.ma.masked:
+                values.append((start_result, start_edge))
         # floor the start position as it will have been floored during the slice
         value_index = operator(array[search_slice]) + floor(search_slice.start or 0) * (search_slice.step or 1)
         value = array[value_index]
-        indexes = (start_idx, value_index, stop_idx)
-        values = (start_result, value, stop_result)
-        values_mask = [True if x is np.nan else False for x in values]
-        # test start / stop edgge
-        index = operator(np.ma.array(data=values, mask=values_mask))
-
-        return Value(indexes[index], values[index])
+        values.append((value, value_index))
+        if stop_edge:
+            stop_result = value_at_index(array, stop_edge)
+            if stop_result is not None and stop_result is not np.ma.masked:
+                values.append((stop_result, stop_edge))
+        result_idx = operator(np.ma.array(values)[:,0])
+        return Value(values[result_idx][1], values[result_idx][0])
     
     else:
         return Value(None, None)

tests/derived_parameters_test.py
@@ -5249,7 +5249,7 @@ class TestVref(unittest.TestCase, NodeTest):
     def setUp(self):
         self.node_class = Vref
         self.airspeed = P('Airspeed', np.ma.repeat(200, 128))
-        self.approaches = buildsections('Approach And Landing', (2, 42), (66, 106))
+        self.approaches = buildsections('Approach And Landing', (2, 41), (66, 105))
 
     def test_can_operate(self):
         # AFR:
@@ -5342,7 +5342,7 @@ class TestVrefLookup(unittest.TestCase, NodeTest):
             KeyTimeInstance(name='Touchdown', index=7),
             KeyTimeInstance(name='Touchdown', index=71),
         ])
-        self.approaches = buildsections('Approach And Landing', (2, 42), (66, 106))
+        self.approaches = buildsections('Approach And Landing', (2, 41.1), (66, 105.1))
 
     @patch('analysis_engine.library.at')
     def test_can_operate(self, at):
@@ -5454,7 +5454,7 @@ class TestVapp(unittest.TestCase, NodeTest):
     def setUp(self):
         self.node_class = Vapp
         self.airspeed = P('Airspeed', np.ma.repeat(200, 128))
-        self.approaches = buildsections('Approach And Landing', (2, 42), (66, 106))
+        self.approaches = buildsections('Approach And Landing', (2, 41), (66, 105))
 
     def test_can_operate(self):
         # AFR:
@@ -5547,7 +5547,7 @@ class TestVappLookup(unittest.TestCase, NodeTest):
             KeyTimeInstance(name='Touchdown', index=7),
             KeyTimeInstance(name='Touchdown', index=71),
         ])
-        self.approaches = buildsections('Approach And Landing', (2, 42), (66, 106))
+        self.approaches = buildsections('Approach And Landing', (2, 41), (66, 105))
 
     @patch('analysis_engine.library.at')
     def test_can_operate(self, at):
@@ -6274,7 +6274,7 @@ class TestAirspeedMinusVref(unittest.TestCase, NodeTest):
         self.airspeed = P('Airspeed', np.ma.repeat(102, 2000))
         self.vref_record = P('Vref', np.ma.repeat((90, 120), 1000))
         self.vref_lookup = P('Vref Lookup', np.ma.repeat((95, 125), 1000))
-        self.approaches = buildsection('Approach And Landing', 500, 1000)
+        self.approaches = buildsection('Approach And Landing', 500, 999)
 
     def test_derive__record_only(self):
         node = self.node_class()
@@ -6378,7 +6378,7 @@ class TestAirspeedMinusVapp(unittest.TestCase, NodeTest):
         self.airspeed = P('Airspeed', np.ma.repeat(102, 2000))
         self.vapp_record = P('Vapp', np.ma.repeat((90, 120), 1000))
         self.vapp_lookup = P('Vapp Lookup', np.ma.repeat((95, 125), 1000))
-        self.approaches = buildsection('Approach And Landing', 500, 1000)
+        self.approaches = buildsection('Approach And Landing', 500, 999)
 
     def test_derive__record_only(self):
         node = self.node_class()