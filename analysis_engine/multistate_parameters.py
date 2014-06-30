20cdf0085a6907a16d2fc39ec6970f335a00954f
Marek Petlicki <marek.petlicki@flightdataservices.com>
2014-06-30 14:11:05 +0100

    Gear Down Selected should use the Gear Down mask

--- analysis_engine/multistate_parameters.py
+++ analysis_engine/multistate_parameters.py
@@ -1318,6 +1318,7 @@ class GearDownSelected(MultistateDerivedParameterNode):
 
         self.array = np.ma.zeros(gear_down.array.size, dtype=np.short)
         self.array[gear_down.array == 'Down'] = 'Down'
+        self.array.mask = gear_down.array.mask
         if gear_warn:
             # We use up to 10s of `Gear (*) Red Warning` == 'Warning'
             # preceeding the actual gear position change state to define the
