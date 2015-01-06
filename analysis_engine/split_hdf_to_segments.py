d3afbe59faac8962167529a31ebb486b5af0cf51
Rob Parker <robert.parker@flightdataservices.com>
2015-01-06 14:02:56 +0000

    fixed missing assignment

--- analysis_engine/split_hdf_to_segments.py
+++ analysis_engine/split_hdf_to_segments.py
@@ -109,7 +109,7 @@ def _segment_type_and_slice(airspeed_array, airspeed_frequency, heading_array,
 
     # Check Heading
     if eng_arrays is not None:
-        np.ma.masked_where(eng_arrays[heading_start:heading_stop] < 1, heading_array)
+        heading_array = np.ma.masked_where(eng_arrays[heading_start:heading_stop] < 1, heading_array)
     hdiff = np.ma.abs(np.ma.diff(heading_array)).sum()
 
     heading_change = hdiff > settings.HEADING_CHANGE_TAXI_THRESHOLD
