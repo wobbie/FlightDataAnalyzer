library.straighten_overflows: fix idx for overflow
Fixed idx calculated for overflows to be relative to the slice in which
it was calculated, otherwise we trigger overflows earlier than they
should be.

analysis_engine/library.py
@@ -6900,7 +6900,7 @@ def straighten_overflows(array, min_val, max_val, threshold=4):
         for idx in np.where(np.abs(diff) > total_range - partition)[0]:
             sign = np.sign(diff[idx])
             overflow -= sign
-            straight[idx + 1:] -= sign * total_range
+            straight[unmasked_slice.start + idx + 1:] -= sign * total_range
         
         last_value = array[unmasked_slice.stop - 1]
     