9e1258b1103e2dcc0b9c5b6cfca4a24cc1ed154d
Rob Parker <robert.parker@flightdataservices.com>
2015-06-08 10:38:32 +0100

    A330 speedbrakes

--- analysis_engine/derived_parameters.py
+++ analysis_engine/derived_parameters.py
@@ -5984,7 +5984,7 @@ class Speedbrake(DerivedParameterNode):
                 'Spoiler (4)' in available and
                 'Spoiler (8)' in available
             ) or
-            family_name in ['B737 Classic', 'A319', 'A320', 'A321', 'Global'] and (
+            family_name in ['B737 Classic', 'A319', 'A320', 'A321', 'A330', 'Global'] and (
                 'Spoiler (2)' in available and
                 'Spoiler (7)' in available
             ) or
@@ -6056,7 +6056,7 @@ class Speedbrake(DerivedParameterNode):
             self.merge_spoiler(spoiler_4, spoiler_9)
         elif family_name == 'A318':
             self.merge_spoiler(spoiler_4, spoiler_8)
-        elif family_name in ['B737 Classic', 'A319', 'A320', 'A321', 'Global']:
+        elif family_name in ['B737 Classic', 'A319', 'A320', 'A321', 'A330', 'Global']:
             self.merge_spoiler(spoiler_2, spoiler_7)
 
         elif family_name == 'B787':
